431b1eb2368873494b01ecbde3d087675ec6ad1e
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-01-28 15:16:04 +0100

    - Add BucketList

---
+++ 10-Project-BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Eric on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 10-Project-BucketList/BucketList/Views/EditView.swift
@@ -0,0 +1,63 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Eric on 24/01/2024.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var viewModel: ViewModel
+    var onSave: (Location) -> Void
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Place Name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
+                }
+                
+                Section("Nearby...") {
+                    switch viewModel.loadingState {
+                    case .loading:
+                        Text("Loading...")
+                    case .loaded:
+                        ForEach(viewModel.pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text(page.description)
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
+            }
+            .navigationTitle("Place details")
+            .toolbar {
+                Button("Save") {
+                    let newLocation = viewModel.createNewLocation()
+                    onSave(newLocation)
+                    dismiss()
+                }
+            }
+            .task {
+                await viewModel.fetchNearbyPlaces()
+            }
+        }
+    }
+    
+    init(location: Location, onSave: @escaping (Location) -> Void) {
+        self.onSave = onSave
+        _viewModel = State(initialValue: ViewModel(location: location))
+      
+    }
+}
+
+#Preview {
+    EditView(location: .example) { _ in }
+}
